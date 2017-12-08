6e514a6a64283bcacc4a118c77823a82e2e6acf6
garsim <garsim@users.noreply.github.com>
2017-12-08 18:01:29 +0100

    Update basichttpdownloader.py

--- basichttpdownloader.py
+++ basichttpdownloader.py
@@ -2,7 +2,7 @@ import urllib
 
 testfile = urllib.URLopener()
 try:
-	testfile.retrieve("http://192.168.56.101/file.gz", "file.gz")
+	testfile.retrieve("http://www.testsite.com/file.gz", "file.gz")
 	print("file.gz - HTTP Download OK")
 except:
 	print("Error in HTTP Download")
